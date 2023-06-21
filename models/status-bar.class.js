df6bce291d4d9246a76bc9aec9fdea69b70fa61c
Björn Daigger <124874016+bjoerndaigger@users.noreply.github.com>
2023-06-21 15:50:47 +0200

    backup

--- models/status-bar.class.js
+++ models/status-bar.class.js
@@ -1,18 +1,18 @@
-class StatusBar {
+class StatusBar extends DrawableObject {
     IMAGES = [
         '../img/7_statusbars/1_statusbar/2_statusbar_health/blue/0.png',
         '../img/7_statusbars/1_statusbar/2_statusbar_health/blue/20.png',
         '../img/7_statusbars/1_statusbar/2_statusbar_health/blue/40.png',
         '../img/7_statusbars/1_statusbar/2_statusbar_health/blue/60.png',
-        '../img/7_statusbars/1_statusbar/2_statusbar_health/blue/80.png', // 4
-        '../img/7_statusbars/1_statusbar/2_statusbar_health/blue/100.png' // 5
+        '../img/7_statusbars/1_statusbar/2_statusbar_health/blue/80.png', 
+        '../img/7_statusbars/1_statusbar/2_statusbar_health/blue/100.png' 
     ];
 
     percentage = 100;
 
     constructor() {
         this.loadImages(this.IMAGES);
-        console.log(loadImages);
+
     }
 
     setPercentage(percentage) {
