72424df8240cbb4d956db6cae4dcefc6c8af4cce
Björn Daigger <124874016+bjoerndaigger@users.noreply.github.com>
2023-07-22 20:24:26 +0200

    show bottle

---
+++ models/bottle.class.js
@@ -0,0 +1,11 @@
+class Bottle extends MovableObject {
+y = 350;
+x = 350;
+width = 100;
+height = 100;
+
+constructor() {
+    super().loadImage('img/6_salsa_bottle/1_salsa_bottle_on_ground.png');
+
+}
+}
